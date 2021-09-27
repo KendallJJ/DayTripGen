2f0361d0bf02346708a2da1085c42026a4f75d02
Kendall Jacobs <kendalljjacobs@icloud.com>
2021-09-27 09:20:15 -0500

    Add else if statements to clarify what the code needs to do

--- app.js
+++ app.js
@@ -7,47 +7,81 @@
 // Math.floor(Math.random() * 3); returns random interger
 
 
-let destination = ['Statue of Liberty', 'Time Square', 'Empire State Building']
 let userinput;
 
 // userinput = prompt("Where am I going this Weekend?")
 let randomDestination = (max) => Math.floor(Math.random()* max);
 
-function chosenDestination(){
-    let firstPlace = destination[Math.floor(Math.random()* 3)]
-        console.log(firstPlace);
-    return firstPlace
-        
+function choseItem(someArray){
+    let result = someArray[Math.floor(Math.random()* someArray.length)]
+    console.log(result);
+    return result
+   
+    
 }
-chosenDestination();
 
+let destination = ['Statue of Liberty', 'Time Square', 'Empire State Building', "Grand Canyon"]
+let firstDestination = choseItem(destination);
 
 let restaurants = ['Sushi Noz', 'Birria-Landia', 'Ramen-Ya'];
+let firstRestaurant = choseItem(restaurants);
 
-function chosenRestaurant(){
-    let firstRestaurant = restaurants[Math.floor(Math.random()* 3)]
-    console.log(firstRestaurant);
-    return firstRestaurant
-}
-        chosenRestaurant();
+
+
 
 let transportation = ['Uber', 'Lyft', 'Taxi'];
+let firstTransportation = choseItem(transportation);
 
-function chosenTransportation(){
-    let firstTransportation = transportation[Math.floor(Math.random()* 3)]
-    console.log(firstTransportation)
-}
-       chosenTransportation();
+
+   
 
 let entertainment = ['Nets Game', 'Knicks Game', 'Dodgers Game'];
+let firstEntertainment = choseItem(entertainment);
 
-    function chosenEntertainment(){
-        let firstEntertainment = entertainment[Math.floor(Math.random()* 3)]
-        console.log(firstEntertainment)
-       
-    }
 
-        chosenEntertainment();
-        
-        alert(destination + restaurants + transportation + entertainment);
-        
+let dayTrip = firstEntertainment + " " + firstDestination + " " + firstRestaurant + " " + firstTransportation;
+let counter = 1;
+while(dayTrip <= dayTrip){
+    alert("Hi your trip for to day includes"+" " + dayTrip);
+    userinput = prompt("Are you satisfied with your Day Trip?")
+
+    dayTrip++;
+if(userinput == "Yes"){
+    alert("Have a good trip!");
+    
+}
+else if(userinput == "No"){
+    alert("Sorry to hear that.")
+    alert("Hi your trip for to day includes"+" " + dayTrip);
+} 
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+// random gen first options
+
+// display results: tools - loop - while
+// Ask for satisfaction? : tools - prompt
+// if yes, display final results : tools - alert and console.log
+// if no, prompt for choice to change: prompt
+// then re-gen result: tools - functions written
+
+
+// let notSatisfied = true
+// while (notSatisfied){
+//     // prompt for satisfaction
+//     if ... yes 
+//     notSatisfied = !notSatisfied
+// }
